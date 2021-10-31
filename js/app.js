cd76226a8e55f467729eeed852883312d1f451c8
vzhabchyk <rvromanovna@gmail.com>
2021-10-31 16:44:42 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const categoryList = document.getElementsByClassName('category-list')[0];
+const productsList = document.getElementsByClassName('products-list')[0];
+const productDescription = document.getElementsByClassName('product-description')[0];
+const btnBuy = document.getElementsByClassName('btn-buy')[0];
+
+for (let i = 0; i < arrayCategory.length; i++) {
+  const itemCategory = document.createElement('li');
+  itemCategory.innerHTML = arrayCategory[i].name;
+  categoryList.appendChild(itemCategory);
+  itemCategory.addEventListener('click', function () {
+    productsList.innerHTML = '';
+    productDescription.innerHTML = '';
+    btnBuy.classList.add('btn-hidden');
+    for (let j = 0; j < arrayCategory[i].products.length; j++) {
+      const itemProduct = document.createElement('li');
+      itemProduct.innerHTML = arrayCategory[i].products[j].name;
+      productsList.appendChild(itemProduct);
+      itemProduct.addEventListener('click', function() {
+        productDescription.innerHTML = arrayCategory[i].products[j].description;
+        btnBuy.classList.remove('btn-hidden');
+      })
+    }
+  })
+}
+
+function buyProduct () {
+  alert('Item bought.');
+  btnBuy.classList.add('btn-hidden');
+  productDescription.innerHTML = '';
+  productsList.innerHTML = '';
+}
+
---
+++ js/data.js
@@ -0,0 +1,57 @@
+const arrayCategory = [
+  {
+    name: 'Phone',
+    products: [
+      {
+        name: 'IPhone 13',
+        description: 'Экран (5.4", OLED (Super Retina XDR), 2340х1080) / Apple A15 Bionic / двойная основная камера: 12 Мп + 12 Мп, фронтальная камера: 12 Мп / 128 ГБ встроенной памяти / 3G / LTE / 5G / GPS / Nano-SIM, eSIM / iOS 15.',
+      },
+      {
+        name: 'Samsung S20',
+        description: 'Экран (6.7", Super AMOLED Plus, 2400x1080) / Samsung Exynos 990 (2 x 2.73 ГГц + 2 x 2.5 ГГц + 4 x 2.0 ГГц) / тройная основная камера: 12 Мп + 12 Мп + 64 Мп, фронтальная: 10 Мп / RAM 8 ГБ / 256 ГБ встроенной памяти / 3G / LTE / GPS / поддержка 2х SIM-карт (Nano-SIM) / Android 10 / 4300 мА*ч.',
+      },
+      {
+        name: 'Nokia 12',
+        description: 'Оригинальный Nokia 6310 в своё время завоевал огромную популярность среди пользователей благодаря строгому дизайну и высокой надёжности. Спустя двадцать лет HHD Global решила перевыпустить классическое устройство.',
+      },
+    ]
+  },
+  {
+    name: 'Notebook',
+    products: [
+      {
+        name: 'Macbook',
+        description: 'Дисплей Retina 13,3 дюйма с широким цветовым охватом P3 для потрясающей цветопередачи и невероятной детализации.',
+      },
+      {
+        name: 'Lenovo',
+        description: 'Экран 15.6” IPS (1920x1080) Full HD, матовый / AMD Ryzen 5 5500U (2.1 — 4.0 ГГц) / RAM 16 ГБ / SSD 512 ГБ / AMD Radeon Graphics / без ОД / Wi-Fi / Bluetooth / веб-камера / DOS / 1.75 кг / серебристый.',
+      },
+      {
+        name: 'Asus',
+        description: 'Экран 15.6" IPS (1920x1080) Full HD 144 Гц, матовый / Intel Core i7-11800H (2.3 - 4.6 ГГц) / RAM 16 ГБ / SSD 512 ГБ / nVidia GeForce RTX 3050, 4 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / без ОС / 2.3 кг / серый.',
+      },
+      {
+        name: 'HP',
+        description: 'Экран 15.6” IPS (1920x1080) Full HD, матовый / AMD Ryzen 5 5500U (2.1 - 4.0 ГГц) / RAM 16 ГБ / SSD 512 ГБ / AMD Radeon Graphics / без ОД / Wi-Fi / Bluetooth / веб-камера / DOS / 1.69 кг / серебристый.',
+      },
+    ]
+  },
+  {
+    name: 'TV',
+    products: [
+      {
+        name: 'Samsung',
+        description: '100% охват цвета благодаря технологии квантовых точек обеспечивает наше лучшее изображение за всю нашу историю.',
+      },
+      {
+        name: 'Philips',
+        description: 'Процессор Philips P5: превосходное качество с любого источника..',
+      },
+      {
+        name: 'Panasonic',
+        description: 'Технологія Dolby Vision IQ використовує вдосконалений датчик розсіяного світла телевізора.',
+      },
+    ]
+  },
+];
