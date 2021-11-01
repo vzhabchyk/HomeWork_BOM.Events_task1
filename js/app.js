2599a5f6bb38bcdc4cd8d0658d2b09b132ddeeff
vzhabchyk <rvromanovna@gmail.com>
2021-11-01 21:41:39 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -3,25 +3,28 @@ const productsList = document.getElementsByClassName('products-list')[0];
 const productDescription = document.getElementsByClassName('product-description')[0];
 const btnBuy = document.getElementsByClassName('btn-buy')[0];
 
-for (let i = 0; i < arrayCategory.length; i++) {
-  const itemCategory = document.createElement('li');
-  itemCategory.innerHTML = arrayCategory[i].name;
-  categoryList.appendChild(itemCategory);
-  itemCategory.addEventListener('click', function () {
-    productsList.innerHTML = '';
-    productDescription.innerHTML = '';
-    btnBuy.classList.add('btn-hidden');
-    for (let j = 0; j < arrayCategory[i].products.length; j++) {
-      const itemProduct = document.createElement('li');
-      itemProduct.innerHTML = arrayCategory[i].products[j].name;
-      productsList.appendChild(itemProduct);
-      itemProduct.addEventListener('click', function() {
-        productDescription.innerHTML = arrayCategory[i].products[j].description;
-        btnBuy.classList.remove('btn-hidden');
-      })
-    }
-  })
+function showCategories () {
+  for (let i = 0; i < arrayCategory.length; i++) {
+    const itemCategory = document.createElement('li');
+    itemCategory.innerHTML = arrayCategory[i].name;
+    categoryList.appendChild(itemCategory);
+    itemCategory.addEventListener('click', function () {
+      productsList.innerHTML = '';
+      productDescription.innerHTML = '';
+      btnBuy.classList.add('btn-hidden');
+      for (let j = 0; j < arrayCategory[i].products.length; j++) {
+        const itemProduct = document.createElement('li');
+        itemProduct.innerHTML = arrayCategory[i].products[j].name;
+        productsList.appendChild(itemProduct);
+        itemProduct.addEventListener('click', function() {
+          productDescription.innerHTML = arrayCategory[i].products[j].description;
+          btnBuy.classList.remove('btn-hidden');
+        })
+      }
+    })
+  }
 }
+showCategories();
 
 function buyProduct () {
   alert('Item bought.');
